7b30aaaf9d3474c5fb25f5c6757a6cca22e5aba6
Victor Gonco <victorgoncarmo@gmail.com>
2021-11-06 16:07:50 -0300

    DonutChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 import Chart from 'react-apexcharts';
 import { BASE_URL } from 'utils/requests';
 import { SaleSum } from 'types/sale';
+import { useEffect, useState } from 'react';
 
 type ChartData = {    
     labels: string[];
@@ -10,19 +11,20 @@ type ChartData = {
 
 
 const DonutChart = () => {
-    //Forma errada de declaração errada, em breve verei os hooks que fazem esse trabalho da forma certa
-    let chartData : ChartData = { labels: [], series: []};
-    //forma errada
-    axios.get (`${BASE_URL}/sales/amount-by-seller`)
-    .then (response => {
-        const data = response.data as SaleSum[];
-        const myLabels = data.map (x => x.sellerName);
-        const mySeries = data.map (x => x.sum);
+    const [chartData, setChartData] = useState<ChartData> ({ labels: [], series: []});
+    useEffect (() => {
+        axios.get (`${BASE_URL}/sales/amount-by-seller`)
+        .then (response => {
+            const data = response.data as SaleSum[];
+            const myLabels = data.map (x => x.sellerName);
+            const mySeries = data.map (x => x.sum);
+    
+    
+            setChartData ({labels : myLabels, series : mySeries});
 
-
-        chartData = {labels : myLabels, series : mySeries};
-        console.log(chartData)
-    });
+        });
+    }, []);  
+    
     /*
     const mockData = {
         series: [477138, 499928, 444867, 220426, 473088],
